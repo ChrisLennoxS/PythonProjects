bd6b4716f3b1c108712966f0f29e9ac97e57ad78
Lennox Scott <chrislennoxs@hotmail.com>
2020-10-01 01:31:05 -0700

    Update Scott_Lab1.py

--- Scott_Lab1.py
+++ Scott_Lab1.py
@@ -1,3 +1,12 @@
+# ------------------------------------------
+# EE 381 Fall 2020 project 3 - Part 2
+# Name: Lennox Scott
+# I.D. #: 017071819
+# Start Date: 09-23-2020
+# End Date:
+# Using a Bernoulli RV to simulate a discrete Markov Chain
+# ------------------------------------------
+
 # Shift Cipher
 
 
